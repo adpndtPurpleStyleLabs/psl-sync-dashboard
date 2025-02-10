da12f98d540fdf3cc12be7a395421e93dfa2aa93
Aditya.Pandit <adpndt@gmail.com>
2025-02-10 11:03:48 +0530

    Update LoginApplication.java

--- src/main/java/com/example/Login/LoginApplication.java
+++ src/main/java/com/example/Login/LoginApplication.java
@@ -11,5 +11,15 @@ public class LoginApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginApplication.class, args);
+		long heapSize = Runtime.getRuntime().totalMemory();
+		long maxHeapSize = Runtime.getRuntime().maxMemory();
+		long freeHeapSize = Runtime.getRuntime().freeMemory();
+
+		System.out.println("=================================");
+		System.out.println("JVM Heap Memory Details:");
+		System.out.println("Initial Heap Size  (Total Memory) : " + (heapSize / 1024 / 1024) + " MB");
+		System.out.println("Max Heap Size      : " + (maxHeapSize / 1024 / 1024) + " MB");
+		System.out.println("Free Heap Size     : " + (freeHeapSize / 1024 / 1024) + " MB");
+		System.out.println("=================================");
 	}
 }
